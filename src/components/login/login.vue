4b0921d88fea37704cb83c1af8704c1365d315e2
insense1106 <insense11061106@163.com>
2020-03-15 10:01:46 +0800

    edit 315

---
+++ src/components/login/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>login</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="stylus" scoped></style>
